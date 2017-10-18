1da99aecb058b8ef7f945de35635f397ba2aad16
Hugo Picado <hugo.pcd@gmail.com>
2017-10-18 20:48:43 +0100

    Refactoring project structure
    Refactor project structure to comply with the one defined below:
    
    - https://github.com/dotnet/docs/blob/master/docs/core/porting/project-structure.md
    
    <Solution>
    src/<Project>
    tests/<Project>.Tests

--- integration-tests-with-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker run -it -d --name mockserver_it_tests -p 1080:1080 jamesdbloom/mockserver
-dotnet test Src/MockServerClientCSharp.Tests
-docker kill mockserver_it_tests
-docker rm mockserver_it_tests
