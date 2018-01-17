558f51a9daa230f51d2e7cde662d9732bbecadfe
Bruno Santos <bruno.santos@farfetch.com>
2018-01-17 14:41:36 +0000

    Load Expectations from file support

---
+++ src/MockServerClientCSharp/Extensions/MockServerExtensions.cs
@@ -0,0 +1,80 @@
+﻿namespace MockServerClientCSharp.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using MockServerClientCSharp.Model;
+    using Newtonsoft.Json;
+
+    public static class MockServerExtensions
+    {
+        public static void LoadExpectationsFromFile(this MockServerClient mockServerClient, string expectationsFilePath)
+        {
+            if (!File.Exists(expectationsFilePath))
+            {
+                throw new FileNotFoundException($"File: {expectationsFilePath} not found!");
+            }
+
+            var fileContent = File.ReadAllText(expectationsFilePath);
+            var expectations = JsonConvert.DeserializeObject<IEnumerable<Expectation>>(fileContent);
+
+            if (expectations.Any())
+            {
+                foreach (var expectation in expectations)
+                {
+                    var httpRequest = HttpRequest.Request();
+                    var httpResponse = HttpResponse.Response();
+
+                    SetupMockServerRequest(mockServerClient, expectation, httpRequest, httpResponse);
+                }
+            }
+        }
+
+        private static void SetupMockServerRequest(MockServerClient mockServerClient, Expectation expectation, HttpRequest httpRequest, HttpResponse httpResponse)
+        {
+            var isSecure = expectation.HttpRequest.IsSecure.HasValue && expectation.HttpRequest.IsSecure.Value;
+            var unlimitedTimes = expectation.Times == null || expectation.Times.IsUnlimited;
+
+            mockServerClient
+                .When(httpRequest
+                        .WithMethod(expectation.HttpRequest.Method)
+                        .WithPath(expectation.HttpRequest.Path)
+                        .WithQueryStringParameters(expectation.HttpRequest.Parameters.ToArray())
+                        .WithBody(expectation.HttpRequest.Body)
+                        .WithSecure(isSecure),
+                        unlimitedTimes ? Times.Unlimited() : Times.Exactly(expectation.Times.Count))
+                .Respond(httpResponse
+                         .WithStatusCode(expectation.HttpResponse.StatusCode)
+                         .WithHeaders(expectation.HttpResponse.Headers.ToArray())
+                         .WithBody(expectation.HttpResponse.Body)
+                         .WithDelay(GetTimeSpanDelay(expectation.HttpResponse.Delay)));
+        }
+
+        private static TimeSpan GetTimeSpanDelay(Delay delay)
+        {
+            if (delay == null)
+            {
+                return TimeSpan.FromSeconds(0);
+            }
+
+            switch (delay.TimeUnit)
+            {
+                case "MILLISECONDS":
+                    return TimeSpan.FromMilliseconds(delay.Value);
+                case "SECONDS":
+                    return TimeSpan.FromSeconds(delay.Value);
+                case "MINUTES":
+                    return TimeSpan.FromMinutes(delay.Value);
+                case "HOURS":
+                    return TimeSpan.FromHours(delay.Value);
+                case "DAYS":
+                    return TimeSpan.FromDays(delay.Value);
+                case "TICKS":
+                    return TimeSpan.FromTicks(delay.Value);
+                default:
+                    return TimeSpan.FromSeconds(0);
+            }
+        }
+    }
+}
--- tests/MockServerClientCSharp.Tests/MockServerClientTest.cs
+++ tests/MockServerClientCSharp.Tests/MockServerClientTest.cs
@@ -1,16 +1,19 @@
 namespace MockServerClientCSharp.Tests
 {
-  using System;
-  using System.Net;
-  using System.Net.Http;
-  using MockServerClientCSharp.Model;
-  using MockServerClientCSharp.Verify;
-  using Xunit;
-  using static MockServerClientCSharp.Model.HttpRequest;
-  using static MockServerClientCSharp.Model.HttpResponse;
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Net.Http;
+    using MockServerClientCSharp.Extensions;
+    using MockServerClientCSharp.Model;
+    using MockServerClientCSharp.Verify;
+    using Newtonsoft.Json;
+    using Xunit;
+    using static MockServerClientCSharp.Model.HttpRequest;
+    using static MockServerClientCSharp.Model.HttpResponse;
 
-  public class MockServerClientTest: IDisposable
-  {
+    public class MockServerClientTest: IDisposable
+    {
     private readonly string MockServerHost = Environment.GetEnvironmentVariable("MOCKSERVER_TEST_HOST") ?? "localhost";
     private readonly int MockServerPort = int.Parse(Environment.GetEnvironmentVariable("MOCKSERVER_TEST_PORT") ?? "1080");
 
@@ -18,221 +21,240 @@ namespace MockServerClientCSharp.Tests
 
     public MockServerClientTest()
     {
-      mockServerClient = new MockServerClient(MockServerHost, MockServerPort);
+        mockServerClient = new MockServerClient(MockServerHost, MockServerPort);
     }
 
     public void Dispose()
     {
-      mockServerClient.Reset();
+        mockServerClient.Reset();
     }
 
     [Fact]
     public void ShouldRespondAccordingToExpectation()
     {
-      // arrange
-      SetupLoginResponse(unlimited: true);
+        // arrange
+        SetupLoginResponse(unlimited: true);
 
-      // act
-      string responseBody = null;
-      HttpStatusCode? statusCode = null;
+        // act
+        string responseBody = null;
+        HttpStatusCode? statusCode = null;
 
-      SendRequest(BuildLoginRequest(), out responseBody, out statusCode);
+        SendRequest(BuildLoginRequest(), out responseBody, out statusCode);
 
-      // assert
-      Assert.Equal(HttpStatusCode.Unauthorized, statusCode.Value);
-      Assert.Equal("{ \"message\": \"incorrect username and password combination\" }", responseBody);
+        // assert
+        Assert.Equal(HttpStatusCode.Unauthorized, statusCode.Value);
+        Assert.Equal("{ \"message\": \"incorrect username and password combination\" }", responseBody);
     }
 
     [Fact]
     public void ShouldRespondAccordingToExpectationOnly2Times()
     {
-      // arrange
-      SetupLoginResponse(unlimited: false, times: 2);
+        // arrange
+        SetupLoginResponse(unlimited: false, times: 2);
 
-      // act 1
-      string responseBody = null;
-      HttpStatusCode? statusCode = null;
-      SendRequest(BuildLoginRequest(), out responseBody, out statusCode);
+        // act 1
+        string responseBody = null;
+        HttpStatusCode? statusCode = null;
+        SendRequest(BuildLoginRequest(), out responseBody, out statusCode);
 
-      // assert
-      Assert.Equal(HttpStatusCode.Unauthorized, statusCode.Value);
-      Assert.Equal("{ \"message\": \"incorrect username and password combination\" }", responseBody);
+        // assert
+        Assert.Equal(HttpStatusCode.Unauthorized, statusCode.Value);
+        Assert.Equal("{ \"message\": \"incorrect username and password combination\" }", responseBody);
 
-      // act 2
-      responseBody = null;
-      statusCode = null;
-      SendRequest(BuildLoginRequest(), out responseBody, out statusCode);
+        // act 2
+        responseBody = null;
+        statusCode = null;
+        SendRequest(BuildLoginRequest(), out responseBody, out statusCode);
 
-      // assert
-      Assert.Equal(HttpStatusCode.Unauthorized, statusCode.Value);
-      Assert.Equal("{ \"message\": \"incorrect username and password combination\" }", responseBody);
+        // assert
+        Assert.Equal(HttpStatusCode.Unauthorized, statusCode.Value);
+        Assert.Equal("{ \"message\": \"incorrect username and password combination\" }", responseBody);
 
-      // act 3
-      responseBody = null;
-      statusCode = null;
-      SendRequest(BuildLoginRequest(), out responseBody, out statusCode);
+        // act 3
+        responseBody = null;
+        statusCode = null;
+        SendRequest(BuildLoginRequest(), out responseBody, out statusCode);
 
-      // assert
-      Assert.Equal(HttpStatusCode.NotFound, statusCode.Value);
+        // assert
+        Assert.Equal(HttpStatusCode.NotFound, statusCode.Value);
     }
 
     [Fact]
     public void ShouldClearExpectation()
     {
-      // arrange
-      HttpRequest request = Request().WithMethod("GET").WithPath("/hello");
+        // arrange
+        HttpRequest request = Request().WithMethod("GET").WithPath("/hello");
 
-      mockServerClient
+        mockServerClient
         .When(request, Times.Unlimited())
         .Respond(Response().WithStatusCode(200).WithBody("hello").WithDelay(TimeSpan.FromSeconds(0)));
 
-      // act 1
-      string responseBody = null;
-      HttpStatusCode? statusCode = null;
+        // act 1
+        string responseBody = null;
+        HttpStatusCode? statusCode = null;
 
-      SendRequest(BuildGetRequest("/hello"), out responseBody, out statusCode);
+        SendRequest(BuildGetRequest("/hello"), out responseBody, out statusCode);
 
-      // assert
-      Assert.Equal(HttpStatusCode.OK, statusCode.Value);
-      Assert.Equal("hello", responseBody);
+        // assert
+        Assert.Equal(HttpStatusCode.OK, statusCode.Value);
+        Assert.Equal("hello", responseBody);
 
-      // act 2
-      mockServerClient.Clear(request);
+        // act 2
+        mockServerClient.Clear(request);
 
-      SendRequest(BuildGetRequest("/hello"), out responseBody, out statusCode);
+        SendRequest(BuildGetRequest("/hello"), out responseBody, out statusCode);
 
-      // assert
-      Assert.Equal(HttpStatusCode.NotFound, statusCode.Value);
+        // assert
+        Assert.Equal(HttpStatusCode.NotFound, statusCode.Value);
     }
 
     [Fact]
     public void ShouldVerify()
     {
-      // arrange
-      HttpRequest request = Request().WithMethod("GET").WithPath("/hello");
+        // arrange
+        HttpRequest request = Request().WithMethod("GET").WithPath("/hello");
 
-      mockServerClient
+        mockServerClient
         .When(request, Times.Unlimited())
         .Respond(Response().WithStatusCode(200).WithBody("hello").WithDelay(TimeSpan.FromSeconds(0)));
 
-      SendRequest(BuildGetRequest("/hello"), out _, out _);
-      SendRequest(BuildGetRequest("/hello"), out _, out _);
+        SendRequest(BuildGetRequest("/hello"), out _, out _);
+        SendRequest(BuildGetRequest("/hello"), out _, out _);
 
-      // act
-      var result = mockServerClient.Verify(Request()
-                                           .WithMethod("GET")
-                                           .WithPath("/hello"), VerificationTimes.Exactly(2));
+        // act
+        var result = mockServerClient.Verify(Request()
+                                            .WithMethod("GET")
+                                            .WithPath("/hello"), VerificationTimes.Exactly(2));
 
-      // assert
-      Assert.NotNull(result);
+        // assert
+        Assert.NotNull(result);
     }
 
     [Fact]
     public void ShouldVerifyMultiple()
     {
-      // arrange
-      HttpRequest request1 = Request().WithMethod("GET").WithPath("/hello");
-      HttpRequest request2 = Request().WithMethod("GET").WithPath("/world");
+        // arrange
+        HttpRequest request1 = Request().WithMethod("GET").WithPath("/hello");
+        HttpRequest request2 = Request().WithMethod("GET").WithPath("/world");
 
-      mockServerClient
+        mockServerClient
         .When(request1, Times.Unlimited())
         .Respond(Response().WithStatusCode(200).WithBody("hello").WithDelay(TimeSpan.FromSeconds(0)));
 
-      mockServerClient
+        mockServerClient
         .When(request2, Times.Unlimited())
         .Respond(Response().WithStatusCode(200).WithBody("world").WithDelay(TimeSpan.FromSeconds(0)));
 
-      SendRequest(BuildGetRequest("/hello"), out _, out _);
-      SendRequest(BuildGetRequest("/world"), out _, out _);
+        SendRequest(BuildGetRequest("/hello"), out _, out _);
+        SendRequest(BuildGetRequest("/world"), out _, out _);
 
-      // act
-      var result = mockServerClient.Verify(request1, request2);
+        // act
+        var result = mockServerClient.Verify(request1, request2);
 
-      // assert
-      Assert.NotNull(result);
+        // assert
+        Assert.NotNull(result);
     }
 
     [Fact]
     public void ShouldVerifyZeroInteractions()
     {
-      // act
-      var result = mockServerClient.VerifyZeroInteractions();
+        // act
+        var result = mockServerClient.VerifyZeroInteractions();
 
-      // assert
-      Assert.NotNull(result);
+        // assert
+        Assert.NotNull(result);
     }
 
     [Fact]
     public void ShouldRetrieveRecordedRequests()
     {
-      // arrange
-      HttpRequest request = Request().WithMethod("GET").WithPath("/hello");
+        // arrange
+        HttpRequest request = Request().WithMethod("GET").WithPath("/hello");
 
-      mockServerClient
+        mockServerClient
         .When(request, Times.Unlimited())
         .Respond(Response().WithStatusCode(200).WithBody("hello").WithDelay(TimeSpan.FromSeconds(0)));
 
-      string responseBody1 = null;
-      string responseBody2 = null;
+        // act
+        string responseBody1 = null;
+        string responseBody2 = null;
 
-      SendRequest(BuildGetRequest("/hello"), out responseBody1, out _);
-      SendRequest(BuildGetRequest("/hello"), out responseBody2, out _);
+        SendRequest(BuildGetRequest("/hello"), out responseBody1, out _);
+        SendRequest(BuildGetRequest("/hello"), out responseBody2, out _);
 
-      // act
-      var result = mockServerClient.RetrieveRecordedRequests(request);
+        
+        var result = mockServerClient.RetrieveRecordedRequests(request);
 
-      // assert
-      Assert.Equal(2, result.Length);
+        // assert
+        Assert.Equal(2, result.Length);
+    }
+
+    [Fact]
+    public void WhenExpectationsAreLoadedFromFile_ShoulRespondFromTheConfiguredRoutes()
+    {
+        // arrange
+        mockServerClient.LoadExpectationsFromFile(@"ConfigurationFiles\Expectations.json");
+
+        // act
+        string responseBody1 = null;
+        string responseBody2 = null;
+
+        SendRequest(BuildGetRequest("/hello?id=1"), out responseBody1, out _);
+        SendRequest(BuildGetRequest("/hello"), out responseBody2, out _);
+
+        // assert
+        Assert.NotNull(responseBody1);
+        Assert.NotNull(responseBody2);
     }
 
     void SendRequest(HttpRequestMessage request, out string responseBody, out HttpStatusCode? statusCode)
     {
-      using (HttpClient client = new HttpClient())
-      using (HttpResponseMessage res = client.SendAsync(request).Result)
-      using (HttpContent content = res.Content)
-      {
+        using (HttpClient client = new HttpClient())
+        using (HttpResponseMessage res = client.SendAsync(request).Result)
+        using (HttpContent content = res.Content)
+        {
         statusCode = res.StatusCode;
         responseBody = content.ReadAsStringAsync().Result;
-      }
+        }
     }
 
     void SetupLoginResponse(bool ssl = false, bool unlimited = true, int times = 0)
     {
-      mockServerClient
+        mockServerClient
         .When(Request()
-              .WithMethod("POST")
-              .WithPath("/login")
-              .WithQueryStringParameters(
+                .WithMethod("POST")
+                .WithPath("/login")
+                .WithQueryStringParameters(
                 new Parameter("returnUrl", "/account"))
-              .WithBody("{\"username\": \"foo\", \"password\": \"bar\"}")
-              .WithSecure(ssl),
-              unlimited ? Times.Unlimited() : Times.Exactly(times))
+                .WithBody("{\"username\": \"foo\", \"password\": \"bar\"}")
+                .WithSecure(ssl),
+                unlimited ? Times.Unlimited() : Times.Exactly(times))
         .Respond(Response()
-                 .WithStatusCode(401)
-                 .WithHeaders(
-                   new Header("Content-Type", "application/json; charset=utf-8"),
-                   new Header("Cache-Control", "public, max-age=86400"))
-                 .WithBody("{ \"message\": \"incorrect username and password combination\" }")
-                 .WithDelay(TimeSpan.FromSeconds(0)));
+                    .WithStatusCode(401)
+                    .WithHeaders(
+                    new Header("Content-Type", "application/json; charset=utf-8"),
+                    new Header("Cache-Control", "public, max-age=86400"))
+                    .WithBody("{ \"message\": \"incorrect username and password combination\" }")
+                    .WithDelay(TimeSpan.FromSeconds(0)));
     }
 
     HttpRequestMessage BuildLoginRequest(bool ssl = false)
     {
-      HttpRequestMessage request = new HttpRequestMessage();
-      request.Method = HttpMethod.Post;
+        HttpRequestMessage request = new HttpRequestMessage();
+        request.Method = HttpMethod.Post;
 
-      string scheme = ssl ? "https" : "http";
-      request.RequestUri = new Uri(scheme + "://" + MockServerHost + ":" + MockServerPort + "/login?returnUrl=/account");
-      request.Content = new StringContent("{\"username\": \"foo\", \"password\": \"bar\"}");
-      return request;
+        string scheme = ssl ? "https" : "http";
+        request.RequestUri = new Uri(scheme + "://" + MockServerHost + ":" + MockServerPort + "/login?returnUrl=/account");
+        request.Content = new StringContent("{\"username\": \"foo\", \"password\": \"bar\"}");
+        return request;
     }
 
     HttpRequestMessage BuildGetRequest(string path)
     {
-      HttpRequestMessage request = new HttpRequestMessage();
-      request.Method = HttpMethod.Get;
-      request.RequestUri = new Uri("http://" + MockServerHost + ":" + MockServerPort + path);
-      return request;
+        HttpRequestMessage request = new HttpRequestMessage();
+        request.Method = HttpMethod.Get;
+        request.RequestUri = new Uri("http://" + MockServerHost + ":" + MockServerPort + path);
+        return request;
     }
-  }
+    }
 }
