a868c54f3e3d257b728d1bdd0e88ba775b8f0369
jacek <jacek.dutkowski@ssginsight.com>
2019-04-04 10:16:26 +0200

    Corrected multipart body handling ( mockserver return body as a string )

--- src/MockServerClientNet/Model/HttpRequest.cs
+++ src/MockServerClientNet/Model/HttpRequest.cs
@@ -2,7 +2,8 @@
 {
   using System.Collections.Generic;
   using System.Linq;
-  using Newtonsoft.Json;
+    using MockServerClientNet.Serializer;
+    using Newtonsoft.Json;
 
   public class HttpRequest
   {
---
+++ src/MockServerClientNet/Serializer/BodyConverter.cs
@@ -0,0 +1,31 @@
+﻿using MockServerClientNet.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MockServerClientNet.Serializer
+{
+    public class BodyConverter : CustomCreationConverter<Body>
+    {
+        public override Body Create(Type objectType)
+        {
+            return new Body("");
+        }
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if(reader.ValueType == typeof(string))
+            {
+                return new Body(reader.Value.ToString());
+            }
+            else
+                return base.ReadJson(reader, objectType, existingValue, serializer);
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(string) || base.CanConvert(objectType);
+        }
+    }
+}
