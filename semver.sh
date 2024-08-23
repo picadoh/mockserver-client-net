bcfa10be5be2b6827e241caeef1778e228c3e44b
picadoh <hugo.pcd@gmail.com>
2024-08-23 19:13:20 +0100

    chore: Fix CI/CD setup

--- semver.sh
+++ semver.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-dotnet tool install -g dotnet-setversion
-setversion $1 $2
+echo "::set-output name=new_release_version::$1"
+dotnet tool install -g dotnet-setversion > /dev/null 2>&1
+setversion $1 $2 > /dev/null 2>&1
 
